688b14ca05f7e2dd69dae1470ac291aea93b3fb7
anastasifade <anastasifade@gmail.com>
2026-03-25 16:59:11 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dto/user/CreateUserDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/user/CreateUserDto.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
-import lombok.Builder;
 import lombok.Value;
 
 import java.time.LocalDate;
